900d4bfdd7e17879497dd5178d9b9349a5c85820
surajsurve4511 <141995340+surajsurve4511@users.noreply.github.com>
2024-03-05 14:55:57 +0530

    Update tut7.py

--- tut7.py
+++ tut7.py
@@ -1,1 +1,22 @@
-elif
+elif 'switch window' in order :
+     pyautogui.keyDown('alt')
+     pyautogui.press('tab')
+     time.sleep(1)
+     pyautogui.keyUp('alt')
+elif 'take a screenshot' in order or 'screenshot' in order :
+     speak('Sir, please tell me the name for this file.')
+     name = takeCommand().lower()
+     speak("please hold the screen for a while')
+     time.sleep(3)
+     img = pyautogui.screenshot()
+     img.save(f"(name).png")
+     speak("screenshot captured !")
+elif 'cpu status' in order :
+     cpu()
+elif 'empty recycle bin' in order :
+     winshell.recycle_bin().empty(confirm=False, show_progress = False , sound = True)
+     speak("recycle bin cleaed")
+
+
+          
+          
