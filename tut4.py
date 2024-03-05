1ddbcc460368a8b3575202ce7cc28c5ee3cf0e08
surajsurve4511 <141995340+surajsurve4511@users.noreply.github.com>
2024-03-05 14:30:46 +0530

    Update tut4.py

--- tut4.py
+++ tut4.py
@@ -3,3 +3,12 @@ elif 'shutdown' is order or 'turn off' in order :
      speak('MAke sure all of your applications are closed')
      time.sleep(5)
      subprocess.call(['shudown','/s'])
+elif 'restart' in order :
+     subprocess.call(['shudown','/r'])
+elif 'hibernate' in order :
+     speak('Hibernating....')
+     subprocess.call(['shutdown','/h'])
+elif 'log off 'in order :
+     speak(' make sure all of your applications are closed before signing out sir !')
+     time.sleep(5)
+     sunprocess.call(['shutdown','/i'])
