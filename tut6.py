f29e33424da20a3906048a0eaff3be101ac23d0c
ayushsarware <159871335+ayushsarware@users.noreply.github.com>
2024-03-05 16:39:39 +0530

    Update tut6.py

--- tut6.py
+++ tut6.py
@@ -1,3 +1,9 @@
 elif 'empty recycle bin' in order:
      winshell.recycle_bin().empty(confirm=False, show_progress = False, sound=True)
      speak("recycle bin recycled")
+elif 'camera' in order:
+     cam()
+
+elif 'exist' in order or 'quit' in order: 
+     speak("Thankyou fo using me sir. have a good day")
+     sys.exit()
