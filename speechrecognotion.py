43807472671b82122d1e831b9fbe2a99dfdbe24d
surajsurve4511 <141995340+surajsurve4511@users.noreply.github.com>
2024-03-05 12:19:48 +0530

    Update speechrecognotion.py

--- speechrecognotion.py
+++ speechrecognotion.py
@@ -1,4 +1,7 @@
 import pyttsx3
+import speech_recognition
+import datetime
+import os 
 enngine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
 engnie.setProperty('voice' , voices[1].1d)
@@ -26,8 +29,44 @@ def username():
     uname=takeCommand()
     speak("Welcome Mister"+uname) 
     speak("How cani help you Sir ?")
+
+def wishMe():
+     hour=int(datetime.datetime.now().hour())
+     if hour>=0 and hour<12:
+          speak("Good Morning Sir")
+     else hour>=12 and hour<18 :
+          speak("Good Afternood Sir ")
+     speak("I am your virtual assistant jarvis .")
+
+
+if __name__=='__main_' :
+     wishMe()
+     username()
+     while True :
+          order=takeCommand().lower()
+
+          if 'how are you ' in order 
+              speak("I am fine , Thankyou for asking ")
+              speak("How are you , Sir ?")
+          elif 'fine' in order or 'good' in order:
+              speak("it's good to know that you are fine.")
+          elif 'who i am ' in order :
+              speak('if you can talk then surely you are aa human . ')
+          elif 'who are you' in order
+              speak('i am your virtual assistant jarvis.')
+          elif ' what is your name ' in order :
+              speak('my frineds call me jarvis .')
+          elif 'open notepad' in order :
+              npath="C:\Windows"
+              os.startfile(npath)
+
+      
+       
+
+
+
     
-if __name__=='__main_'
+
         
     
                           
