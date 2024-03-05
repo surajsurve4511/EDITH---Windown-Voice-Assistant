4af28273150dcfdc0c045493e30fed44ee905762
ayushsarware <159871335+ayushsarware@users.noreply.github.com>
2024-03-05 16:35:02 +0530

    Update tut6part2.py

--- tut6part2.py
+++ tut6part2.py
@@ -17,15 +17,29 @@ def cam() :
             btn_frame = tkinter.Frame(window, background="Black")
             btn_frame.place(x=0,y=0)
 
-            self.btn_snapshot = tkinter.Button(bthn_frame,text="Snapshot", width=20, bg="black", fg="white")
-            self.btn_snapshot.pack(side="left,padx=10,pady=10)
+            self.btn_snapshot = tkinter.Button(btn_frame,text="Snapshot", width=20, bg="black", fg="white")
+            self.btn_snapshot.pack(side="left",padx=10,pady=10)
+
+            self delay = 15
+            self.update()
+
+
+            self.window.mainloop()
 
          def snapshot(self):
              ret, frame = self.vid.get_frame()
              if ret:
-                 cv2.imwrite ("My Caputre
+                 cv2.imwrite ("My Capture "+ time.strftime("%d-%m-%Y-%H-%m-%s")+", jpg",cv2.cvtColor(frame,cv2.COLOR_RGB2BGR))
+                 messagebox.shoeinfo("Notification","Image Saved")
         
+         def update(self):
+             ret, frame =self.vid.get_frame()
 
+             if ret:
+                 self.photo = PIL.Image.Tk.PhotoImage(image=PIL.image.freaquency((frame))
+                 self.canvas.create_image(0,0, iamge=self,photo, anchor=tkinter.NW)
+
+                 self.window.after(self.delay,self.update)
 
 
 
@@ -49,5 +63,8 @@ def cam() :
                 else:
                     return(ret, None)
              else: 
-                 return
+                 return(ret,None)
+
+      App(tkinter.Tk(),"Camera")
+
                  
