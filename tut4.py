2bf17b9388c19483b203fda5cfafef014c8d705a
surajsurve4511 <141995340+surajsurve4511@users.noreply.github.com>
2024-03-05 14:23:41 +0530

    Update tut4.py

--- tut4.py
+++ tut4.py
@@ -1,2 +1,5 @@
 elif 'shutdown' is order or 'turn off' in order :
-     speak('Hold on 
+     speak('Hold on a second sir! Your system is on itsa way to shutdown')
+     speak('MAke sure all of your applications are closed')
+     time.sleep(5)
+     subprocess.call(['shudown','/s'])
