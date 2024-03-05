f47123a087ca7956ddef86baab1cda4dcbf40feb
surajsurve4511 <141995340+surajsurve4511@users.noreply.github.com>
2024-03-05 15:44:43 +0530

    Update tut7.py

--- tut7.py
+++ tut7.py
@@ -22,6 +22,30 @@ elif 'ip' in order :
      host = socket.gethostname()
      ip = socket.gethostname(host)
      speak("your IP address is" + ip )
+elif 'bmi' in order :
+     speak("Please tell your height in centimetres")
+     height =takeCommand()
+     speak("Please tell your weight in kilograms")
+     weight = takeCommand()
+     height = height/100
+     BMI = weight/(height*height)
+     speak("Your body mass index is " + BMI)
+     if (BMI>0) :
+          if(BMI<=16) :
+               speak("You are severly underweight")
+          elif (BMI<=18.5) :
+               speak("You are underweight")
+          elif (BMI<=25):
+               speak("You are healthy")
+          elif (BMI<=30) :
+               speak("You are overwieght")
+          else:
+               speak("You are severly over weight")
+     else:
+          speak("Enter valid details)
+elif 'movie' in order :
+          
+
 
 
           
