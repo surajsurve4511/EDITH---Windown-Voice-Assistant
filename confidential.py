75cd2790c4790c8dccbf0b371d1bc34565dd8ef9
AtharvaPatil7967 <159103434+AtharvaPatil7967@users.noreply.github.com>
2024-03-08 14:50:24 +0530

    Update confidential.py

--- confidential.py
+++ confidential.py
@@ -3,6 +3,13 @@ import speech_recognition as sr
 import webbrowser
 import datetime
 import time
+import os
+import random
+import pyjokes
+import subprocess
+import time
+import pyautogui
+import psutil
 
 engine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
@@ -15,6 +22,13 @@ def username():
     uname=takeCommand()
     speak("Welcome Mister"+uname) 
     speak("How cani help you Sir ?")
+
+def cpu():
+    usage=str(psutil.cpu_percent())
+    speak("CPU is at"+ usage)
+    battery=str(psutil.sensors_battery())
+    speak("Battery is at"+battery)    
+
 
 def wishMe():
     hour = int(datetime.datetime.now().hour)
@@ -59,13 +73,123 @@ if __name__==  '__main__' :
     username()
     while True :
         order=takeCommand().lower()
-        if 'search' in order :
-            order=order.replace("search for", " ")
-            search_querry(order)
-        elif 'open amazon' in order:
-            speak("here you go to amazon sir. happy shopping")
-            webbrowser.open("amazon.in")
+        if 'how are you ' in order:
+              speak("I am fine , Thankyou for asking ")
+              speak("How are you , Sir ?")
+ 
+        elif 'fine' in order or 'good' in order:
+              speak("it's good to know that you are fine.")
+ 
+        elif 'who i am ' in order :
+              speak('if you can talk then surely you are aa human . ')
+ 
+        elif 'who are you' in order:
+              speak('i am your virtual assistant jarvis.')
+ 
+        elif ' what is your name ' in order :
+              speak('my frineds call me jarvis .')
+ 
+        elif 'open notepad' in order :
+              npath="C:\Windows"
+              os.startfile(npath)
+ 
+        elif 'search' in order :
+              order=order.replace("search for", " ")
+              search_querry(order)
+        
+        elif 'play music' in order or 'play songs' in order:
+              music_dir="D:\\Music"
+              songs=os.listdir(music_dir)
+              rd=random.choice(songs)
+              os.startfile(os.path.join(music_dir ,rd))
+
+        elif 'search on wikipedia' in order:
+              speak('Searching...')
+              order=order.replace("wikipedia","")
+              results=wikipedia.summary(order,sentence=2)
+              speak("According to wikipedia")
+              speak(results)
+
+        elif 'open google' in order:
+              speak("here you go to google sir\n")
+              webbrowser.open("nyntra.com")
+
         elif 'open youtube' in order:
-            speak("here you go to youtube sir")
-            webbrowser.open("youtube.com")
-    
+              speak("here you go to youtube sir")
+              webbrowser.open("youtube.com")
+
+        elif 'open amazon' in order:
+              speak("here you go to amazon sir. happy shopping")
+              webbrowser.open("amazon.in")
+ 
+        elif 'open stackoverflow' in order:
+              speak("here you go to stack overflow. happy coding")
+              webbrowser.open("stackoverflow.com")
+        elif 'open myntra' in order :
+            speak("Here you go to myntra sir. Happing shopping \n")
+            webbrowser.open("myntra,com")
+        elif "where is" in order:
+            order=order.replace("Where is" ,"")
+            location+order
+            speak("Locating.....")
+            speak(Location)
+            webbrowser.open("https://www.google.co.in/maps/place/"+Location+"")
+
+        elif "write a note" in order:
+            speak("What should i write , sir?")
+            note=takeCommand()
+            file=open('jarvis,txt','w')
+            speak("Sir, Should I include date and time as well?")
+            sn=takeCommand()
+            if 'yes' or 'sure'or 'yeah' in sn:
+              strTime=datetime.now().strftime("%H:%M:%S")
+              file.write (strTime)
+              file.write(note)
+              speak("Done Sir!")
+            else:
+                file.write(note)
+                speak("Done Sir!")
+
+        elif 'shown note' in order:
+            speak("Showing notes")
+            file=open("jarvis.txt","r")
+            print(file.read())
+            speak(file.read(6))
+
+        elif 'joke' in order:
+            speak(pyjokes.get_joke(Language="e",category="neutral"))
+
+        elif 'the time' in order:           
+            strTime=datetime.now().strftime("%H:%M:%S")
+            speak(f"Well, the time is {strTime}")
+
+        elif 'shutdown' or  'turn off' in order :
+            speak('Hold on a second sir! Your system is on itsa way to shutdown')
+            speak('MAke sure all of your applications are closed')
+            time.sleep(5)
+            subprocess.call(['shudown','/s'])
+        elif 'restart' in order :
+            subprocess.call(['shudown','/r'])
+        elif 'hibernate' in order :
+            speak('Hibernating....')
+            subprocess.call(['shutdown','/h'])
+        elif 'log off 'in order :
+            speak(' make sure all of your applications are closed before signing out sir !')
+            time.sleep(5)
+            sunprocess.call(['shutdown','/i'])    
+        elif 'switch window' in order:
+            pyautogui.keyDown('alt')
+            pyautogui.press('tab')
+            time.sleep(1)
+            pyautogui.keyUp('alt')
+
+        elif 'take a screenshot' in order or 'screeenshot this' in order:
+            speak('Sir, please tell me the name for this file.')
+            name=takeCommand().lower()
+            speak("please hold the screen")
+            time.sleep(3)
+            img=pyautogui.screenshot()
+            img.save(f"{nmae}.png")
+            speak("screenshot captured sir!")
+        elif ' cpu status' in order :
+            cpu()   
